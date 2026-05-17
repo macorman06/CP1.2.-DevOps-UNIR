688f8b75bea5f1d653147adf6636dd89f217e482
Marcos Corpas <marclifford06@gmail.com>
2026-05-17 20:41:21 +0200

    test: split util tests into happy path and error cases

--- test/unit/util_test.py
+++ test/unit/util_test.py
@@ -6,11 +6,13 @@ from app import util
 
 @pytest.mark.unit
 class TestUtil(unittest.TestCase):
-    def test_convert_to_number_correct_param(self):
+    def test_convert_to_number_correct_integer(self):
         self.assertEqual(4, util.convert_to_number("4"))
         self.assertEqual(0, util.convert_to_number("0"))
         self.assertEqual(0, util.convert_to_number("-0"))
         self.assertEqual(-1, util.convert_to_number("-1"))
+
+    def test_convert_to_number_correct_float(self):
         self.assertAlmostEqual(4.0, util.convert_to_number("4.0"), delta=0.0000001)
         self.assertAlmostEqual(0.0, util.convert_to_number("0.0"), delta=0.0000001)
         self.assertAlmostEqual(0.0, util.convert_to_number("-0.0"), delta=0.0000001)
@@ -22,6 +24,7 @@ class TestUtil(unittest.TestCase):
         self.assertRaises(TypeError, util.convert_to_number, "s")
         self.assertRaises(TypeError, util.convert_to_number, None)
         self.assertRaises(TypeError, util.convert_to_number, object())
+
 
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
